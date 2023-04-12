277c8bbf9cd76b9ae8ab16a3624119994599ea55
Gokarna Adhikari <gokarna.adhikari14@gmail.com>
2023-04-12 13:25:24 +0545

    Created the view. ?>?>?

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -7,20 +7,67 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct ContentView: View {
+    @StateObject private var viewModel = CalculatorViewModel()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            VStack(spacing: 12) {
+                Spacer()
+
+                HStack {
+                    Spacer()
+                    Text(viewModel.display)
+                        .font(.system(size: 64))
+                        .foregroundColor(.white)
+                }.padding()
+
+                VStack(spacing: 12) {
+                    HStack(spacing: 12
+                    ) {
+                    CalculatorButtonView(button: .clear).environmentObject(viewModel)
+                    CalculatorButtonView(button: .negative).environmentObject(viewModel)
+                    CalculatorButtonView(button: .percent).environmentObject(viewModel)
+                    CalculatorButtonView(button: .divide).environmentObject(viewModel)
+                    }
+                    HStack(spacing: 12) {
+                        CalculatorButtonView(button: .seven).environmentObject(viewModel)
+                        CalculatorButtonView(button: .eight).environmentObject(viewModel)
+                        CalculatorButtonView(button: .nine).environmentObject(viewModel)
+                        CalculatorButtonView(button: .multiply).environmentObject(viewModel)
+                    }
+
+                    HStack(spacing: 12) {
+                        CalculatorButtonView(button: .four).environmentObject(viewModel)
+                        CalculatorButtonView(button: .five).environmentObject(viewModel)
+                        CalculatorButtonView(button: .six).environmentObject(viewModel)
+                        CalculatorButtonView(button: .minus).environmentObject(viewModel)
+                    }
+
+                    HStack(spacing: 12) {
+                        CalculatorButtonView(button: .one).environmentObject(viewModel)
+                        CalculatorButtonView(button: .two).environmentObject(viewModel)
+                        CalculatorButtonView(button: .three).environmentObject(viewModel)
+                        CalculatorButtonView(button: .plus).environmentObject(viewModel)
+                    }
+
+                    HStack(spacing: 12) {
+                        CalculatorButtonView(button: .zero).environmentObject(viewModel)
+                        CalculatorButtonView(button: .decimal).environmentObject(viewModel)
+                        CalculatorButtonView(button: .equal).environmentObject(viewModel)
+                    }
+                }
+            }.padding(.bottom)
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+static var previews: some View {
+ContentView()
 }
+}
+
