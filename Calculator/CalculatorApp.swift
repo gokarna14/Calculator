6e92313e18f917545bc7e28ee077d3a9e8785228
Gokarna Adhikari <gokarna.adhikari14@gmail.com>
2023-04-12 13:20:33 +0545

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Prabas Adhikari on 12/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
