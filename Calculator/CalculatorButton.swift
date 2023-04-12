f1be62c7e301f1f189648d2394eaeea51021ca7a
Gokarna Adhikari <gokarna.adhikari14@gmail.com>
2023-04-12 13:26:46 +0545

    Created the Button 🥳

---
+++ Calculator/CalculatorButton.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+
+enum CalculatorButton: String {
+    case zero, one, two, three, four, five, six, seven, eight, nine
+    case decimal
+    case equal, plus, minus, multiply, divide
+    case clear, negative, percent
+
+    var title: String {
+        switch self {
+        case .zero: return "0"
+        case .one: return "1"
+        case .two: return "2"
+        case .three: return "3"
+        case .four: return "4"
+        case .five: return "5"
+        case .six: return "6"
+        case .seven: return "7"
+        case .eight: return "8"
+        case .nine: return "9"
+        case .decimal: return "."
+        case .equal: return "="
+        case .plus: return "+"
+        case .minus: return "-"
+        case .multiply: return "x"
+        case .divide: return "÷"
+        case .clear: return "AC"
+        case .negative: return "+/-"
+        case .percent: return "%"
+        }
+    }
+}
