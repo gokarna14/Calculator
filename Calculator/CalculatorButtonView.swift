5e4740c29282597617e8d9cf686da7fac0672214
Gokarna Adhikari <gokarna.adhikari14@gmail.com>
2023-04-12 13:28:13 +0545

    Completed  the Button View

---
+++ Calculator/CalculatorButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  CalculatorButtonView.swift
+//  Calculator
+//
+//  Created by Prabas Adhikari on 12/04/2023.
+//
+
+import SwiftUI
+
+struct CalculatorButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CalculatorButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalculatorButtonView()
+    }
+}
